d8eff1483a7771ade68c72fdeff2f9dea6b156d2
Hola1550 <143151993+Hola1550@users.noreply.github.com>
2026-05-11 14:45:12 -0500

    Create badges.js

---
+++ api/badges.js
@@ -0,0 +1,17 @@
+export default async function handler(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    
+    const { universeId, cursor } = req.query
+    
+    if (!universeId) {
+        return res.status(400).json({ error: "Falta universeId" })
+    }
+    
+    let url = `https://badges.roblox.com/v1/universes/${universeId}/badges?limit=100&sortOrder=Asc`
+    if (cursor) url += `&cursor=${cursor}`
+    
+    const respuesta = await fetch(url)
+    const datos = await respuesta.json()
+    
+    res.status(200).json(datos)
+}
